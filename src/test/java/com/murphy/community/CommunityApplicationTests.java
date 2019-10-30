00385429d1ca40d7532ff83b8fe1d8649efd218d
233 Murphy <zhangyhhust@gmail.com>
2019-10-30 13:21:06 +0800

    init repo

---
+++ src/test/java/com/murphy/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murphy.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
