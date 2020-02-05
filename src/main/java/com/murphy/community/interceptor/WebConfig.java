9d3dd706122fe059418eaaa12f851b2ed7f98d12
finalsatan <zhangyhhust@gmail.com>
2020-02-05 19:03:24 +0800

    modify add interceptor

--- src/main/java/com/murphy/community/interceptor/WebConfig.java
+++ src/main/java/com/murphy/community/interceptor/WebConfig.java
@@ -20,6 +20,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(sessionInterceptor).addPathPatterns("/*");
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
     }
 }
