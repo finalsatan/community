61f8f197283eda69f4627d0daf07552ee5dbe288
finalsatan <zhangyhhust@gmail.com>
2020-02-05 18:58:01 +0800

    delete useless import

--- src/main/java/com/murphy/community/interceptor/WebConfig.java
+++ src/main/java/com/murphy/community/interceptor/WebConfig.java
@@ -2,8 +2,6 @@ package com.murphy.community.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
