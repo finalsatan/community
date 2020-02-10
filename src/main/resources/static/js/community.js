b91d3c1bba3775ae9cf1a02f1bd544331c58b50d
finalsatan <zhangyhhust@gmail.com>
2020-02-10 16:48:00 +0800

    complete jump to login when comment if not login

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -13,7 +13,16 @@ function post() {
             if (response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code == 2003) {
+                    var isAccept = confirm(response.message);
+                    if (isAccept) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=53cd0606229ddef3e6e0&redirect_uri=http://localhost:8887/callback&scope=user&state=edcvfr");
+                        window.localStorage.setItem("closable", "true");
+                    }
+                } else {
+                    alert(response.message);
+                }
+
             }
         },
         dataType: "json",
